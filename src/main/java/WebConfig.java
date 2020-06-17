184d17e93162b9af3f4604f4e8a384e0197a40f7
Dang Pham <thienvu453@gmail.com>
2020-06-17 15:51:17 +0700

    Chuyen doi cau hinh xml sang java class

---
+++ src/main/java/WebConfig.java
@@ -0,0 +1,28 @@
+import cg.wbd.grandemonstration.service.CustomerService;
+import cg.wbd.grandemonstration.service.impl.SimpleCustomerServiceImpl;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configurable
+@EnableWebMvc
+@ComponentScan("cg.wbg.grandemonstration")
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/templates/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public CustomerService customerService() {
+        return new SimpleCustomerServiceImpl();
+    }
+
+}
